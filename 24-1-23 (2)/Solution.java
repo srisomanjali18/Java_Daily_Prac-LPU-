9e593f16fb6a226275adcb1f950d54ee564aa1cc
Sri Somanjali <srisomanjali18@gmail.com>
2023-01-31 21:51:32 +0530

    Day 5 (2)

---
+++ 24-1-23 (2)/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+2
+import java.util.*;
+3
+​
+4
+public class Solution {
+5
+​
+6
+    public static void main(String[] args) {
+7
+        Scanner sc = new Scanner(System.in);
+8
+        int a = sc.nextInt();
+9
+        if(a<=18)
+10
+        {
+11
+            System.out.println("Not eligible");
+12
+        }
+13
+        else{
+14
+            System.out.println("Eligible");
+15
+        }
+16
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+17
+    }
+18
+}
