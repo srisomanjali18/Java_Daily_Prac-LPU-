c55189f7b07145bb993ee6fdacfbe35ca8d7ceee
Sri Somanjali <srisomanjali18@gmail.com>
2023-02-07 20:13:40 +0530

    day 11

---
+++ 7-02-23/Solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt(),low = Integer.MAX_VALUE;
+        
+        if(n < 0)
+        {
+            System.out.print("Invalid Array Size");
+            return;
+        }
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n ; i++)
+        {
+            arr[i] = sc.nextInt();
+            if(arr[i] < low)
+            {
+                low = arr[i];
+            }
+        }
+        
+        System.out.print(low);
+     }
+}
