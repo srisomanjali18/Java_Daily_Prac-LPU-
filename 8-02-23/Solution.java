583878092455fa0438fd07a2c3bc2788694e4cc9
Sri Somanjali <srisomanjali18@gmail.com>
2023-02-08 22:31:40 +0530

    Day 12

--- 8-02-23/Solution.java
+++ 8-02-23/Solution.java
@@ -1,47 +1,29 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-    public static void main(String args[] ) throws Exception {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        if(N>2 && N<30){
-            int arr[] = new int[N];
-            int arr2[] = new int[N];
-            for(int i=0; i<N; i++)
-                arr[i]=sc.nextInt();
-            if(N>2 && N<30){
-                int j = 0;
-                if(arr[0]!=0){
-                    arr2[j++]=arr[0];
-                }
-                else{
-                    System.out.println("Invalid Input");
-                }
-                for(int i=1; i<N; i++){
-                    if(arr[i]!=0){
-                        if(arr[i]!=arr[i-1]){
-                            arr2[j++] = arr[i];
-                        }
-                    }
-                    else{
-                        System.out.println("Invalid Input");
-                    }
-                }
-                for(int i=0; i<j; i++){
-                    System.out.print(arr2[i]+" ");
-                }
-            }
-            
+        
+        int n = sc.nextInt(),low = Integer.MAX_VALUE;
+        
+        if(n < 0)
+        {
+            System.out.print("Invalid Array Size");
+            return;
         }
-        else{
-                System.out.println("Invalid Input");
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < n ; i++)
+        {
+            arr[i] = sc.nextInt();
+            if(arr[i] < low)
+            {
+                low = arr[i];
             }
-            
-            
-    }
+        }
+        
+        System.out.print(low);
+     }
 }
