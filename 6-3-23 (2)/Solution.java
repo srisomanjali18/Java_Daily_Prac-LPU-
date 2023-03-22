af2ab4c854965d9ee788d6e0ad4a221dd27fb567
Sri Somanjali <srisomanjali18@gmail.com>
2023-03-22 21:42:06 +0530

    Day 29 (2)

--- 6-3-23 (2)/Solution.java
+++ 6-3-23 (2)/Solution.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         // System.out.print("Enter password: ");
         String password = input.nextLine();
 
