3031ab5829c49cdbed96f71cbef486b79e477811
Sri Somanjali <srisomanjali18@gmail.com>
2023-03-22 21:36:26 +0530

    Day 30

---
+++ 6-3-23 (2)/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        String convertedPassword = password.toLowerCase().replace(" ", "$");
+
+        System.out.println( convertedPassword);
+    }
+}
